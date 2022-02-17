3dd9238e50d0f61ad7fec9f9b3fcf50f9af0ab0e
giatrung2012 <trungrappar2002@gmail.com>
2022-02-17 16:46:32 +0700

    Updated
    Former-commit-id: 65f81bcde7f6aaaef71c5c63c07b7021c912abb4

---
+++ th-pplthdt/lab3/src/cau1/Entry.java
@@ -0,0 +1,9 @@
+package cau1;
+
+public class Entry {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+        System.out.println(circle.getArea());
+        System.out.println(circle.toString());
+    }
+}
---
+++ th-pplthdt/lab3/src/cau2/Entry.java
@@ -0,0 +1,8 @@
+package cau2;
+
+public class Entry {
+    public static void main(String[] args) {
+        Student student = new Student("ten", "diachi", "chuongtrinh", 2022, 100);
+        System.out.println(student.toString());
+    }
+}
