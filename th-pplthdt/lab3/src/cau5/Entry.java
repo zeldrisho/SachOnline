dbfe2654f989506cbdd4c511252d10f4a6d6dd11
giatrung2012 <trungrappar2002@gmail.com>
2022-02-17 17:06:27 +0700

    Revert "Updated"
    This reverts commit 3dd9238e50d0f61ad7fec9f9b3fcf50f9af0ab0e [formerly 65f81bcde7f6aaaef71c5c63c07b7021c912abb4].
    
    
    Former-commit-id: c3ab7baadbcc5802061b60c3af6d6663c398f765

--- th-pplthdt/lab3/src/cau5/Entry.java
+++
@@ -1,8 +0,0 @@
-package cau5;
-
-public class Entry {
-    public static void main(String[] args) {
-        Employee employee = new Employee("1", "Trung", 2, 2017, 4);
-        System.out.println(employee.getSalary());
-    }
-}
