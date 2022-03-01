30fa913bae980890a1c36ea4f558dc2590942dcc
giatrung2012 <trungrappar2002@gmail.com>
2022-03-01 15:42:01 +0700

    Updated
    Former-commit-id: 013f9e8e6b6efd4e9a5685c0b6e0375696d08926

---
+++ excercises/exercises-on-classes/src/ex7/Date.java
@@ -0,0 +1,51 @@
+package ex7;
+
+public class Date {
+    private int day;
+    private int month;
+    private int year;
+
+    public Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        String sDay = day < 10 ? "0" + day : Integer.toString(day);
+        String sMonth = month < 10 ? "0" + month : Integer.toString(month);
+        String sYear = Integer.toString(year);
+        return sDay + "/" + sMonth + "/" + sYear;
+    }
+}
---
+++ excercises/exercises-on-classes/src/ex8/Time.java
@@ -0,0 +1,87 @@
+package ex8;
+
+public class Time {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    public void setTime(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        String sHour = hour < 10 ? "0" + hour : Integer.toString(hour);
+        String sMinute = minute < 10 ? "0" + minute : Integer.toString(minute);
+        String sSecond = second < 10 ? "0" + second : Integer.toString(second);
+        return sHour + ":" + sMinute + ":" + sSecond;
+    }
+
+    public Time nextSecond() {
+        second++;
+        if (second == 60) {
+            second = 0;
+            if (minute == 59) {
+                minute = 0;
+                if (hour == 23) {
+                    hour = 0;
+                } else {
+                    hour++;
+                }
+            } else {
+                minute++;
+            }
+        }
+        return this;
+    }
+
+    public Time previousSecond() {
+        second--;
+        if (second == -1) {
+            second = 59;
+            if (minute == 0) {
+                minute = 59;
+                if (hour == 0) {
+                    hour = 23;
+                } else {
+                    hour--;
+                }
+            } else {
+                minute--;
+            }
+        }
+        return this;
+    }
+}
