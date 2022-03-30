cdb6e71db2ab9ef8f19cd5614187d5c36ad745f3
giatrung2012 <trungrappar2002@gmail.com>
2022-03-30 16:13:24 +0700

    Updated
    Former-commit-id: 3e9e89c5acc2061972c473a31c0dd16baa3fabfd

--- th-pplthdt/lab5/src/cau5/TestMain.java
+++ th-pplthdt/lab5/src/cau5/TestMain.java
@@ -1,10 +1,13 @@
 package cau5;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class TestMain {
     public static void main(String[] args) {
         Employee emp1 = new Employee("1", "employ1", 10);
         Employee emp2 = new Employee("2", "employ2", 20);
-        System.out.println(emp1);
-        System.out.println(emp2);
+        String pathToInput = "E:\\java\\th-pplthdt\\lab5\\src\\cau5\\data.txt";
+//        Files.write(Paths.get(pathToInput), emp1.getBytes());
     }
 }
