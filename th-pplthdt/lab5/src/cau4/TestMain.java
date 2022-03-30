916aa51c3b968dccacdadbe18cffee1c8dc0b5dd
giatrung2012 <trungrappar2002@gmail.com>
2022-03-30 16:02:44 +0700

    Updated
    Former-commit-id: 6c5633805c2905b70c7c34c6a56a39eb5959e66e

--- th-pplthdt/lab5/src/cau4/TestMain.java
+++ th-pplthdt/lab5/src/cau4/TestMain.java
@@ -1,20 +1,33 @@
 package cau4;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class TestMain {
     public static void main(String[] args) throws IOException {
-        File file = new File("E:\\java\\th-pplthdt\\lab5\\src\\cau4\\test.txt");
-        FileWriter writer = new FileWriter("E:\\java\\th-pplthdt\\lab5\\src\\cau4\\test.txt");
+        String pathToInput = "E:\\java\\th-pplthdt\\lab5\\src\\cau4\\test.txt";
+        File file = new File(pathToInput);
         if (file.exists()) {
             if (file.isDirectory()) {
                 System.out.println("The directory exists");
             } else {
                 System.out.println("The file exists");
-                writer.write("Hello");
+                System.out.println("Append text to an existing file");
+                Files.write(Paths.get(pathToInput), "abc abcdef abcd".getBytes());
 
+                String longestWord = "";
+                String s;
+                Scanner sc = new Scanner(file);
+                while (sc.hasNext()) {
+                    s = sc.next();
+                    if (s.length() > longestWord.length()) {
+                        longestWord = s;
+                    }
+                }
+                System.out.println("The longest word in file is: " + longestWord);
             }
         } else {
             if (file.isDirectory()) {
---
+++ th-pplthdt/lab5/src/cau5/TestMain.java
@@ -0,0 +1,10 @@
+package cau5;
+
+public class TestMain {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("1", "employ1", 10);
+        Employee emp2 = new Employee("2", "employ2", 20);
+        System.out.println(emp1);
+        System.out.println(emp2);
+    }
+}
