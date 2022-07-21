50316504f77b3de752be56c5e694b68e6f731ae3
giatrung2012 <trunghsg2012@gmail.com>
2022-07-21 17:04:33 +0700

    Test
    Former-commit-id: 0ab355786b8e35ee0fe6dd5a2b099ed870a85b70
    Former-commit-id: ced3c605e6c340c9f5d37f695ac4c3e8d43ccc2b

---
+++ windows-programming-labs/2024801030013/Lab51/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace Lab51
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMaNV = new System.Windows.Forms.Label();
+            this.lblTenNV = new System.Windows.Forms.Label();
+            this.lblGioitinh = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblNgaysinh = new System.Windows.Forms.Label();
+            this.lblDiachi = new System.Windows.Forms.Label();
+            this.lblSdt = new System.Windows.Forms.Label();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.rdbNam = new System.Windows.Forms.RadioButton();
+            this.rdbNu = new System.Windows.Forms.RadioButton();
+            this.dtpNgaysinh = new System.Windows.Forms.DateTimePicker();
+            this.wbDiachi = new System.Windows.Forms.WebBrowser();
+            this.btnReport = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.txbSdt = new System.Windows.Forms.TextBox();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblThongtin = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMaNV
+            // 
+            this.lblMaNV.AutoSize = true;
+            this.lblMaNV.Location = new System.Drawing.Point(36, 43);
+            this.lblMaNV.Name = "lblMaNV";
+            this.lblMaNV.Size = new System.Drawing.Size(61, 20);
+            this.lblMaNV.TabIndex = 0;
+            this.lblMaNV.Text = "Mã NV:";
+            this.lblMaNV.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblTenNV
+            // 
+            this.lblTenNV.AutoSize = true;
+            this.lblTenNV.Location = new System.Drawing.Point(31, 94);
+            this.lblTenNV.Name = "lblTenNV";
+            this.lblTenNV.Size = new System.Drawing.Size(66, 20);
+            this.lblTenNV.TabIndex = 1;
+            this.lblTenNV.Text = "Tên NV:";
+            // 
+            // lblGioitinh
+            // 
+            this.lblGioitinh.AutoSize = true;
+            this.lblGioitinh.Location = new System.Drawing.Point(30, 145);
+            this.lblGioitinh.Name = "lblGioitinh";
+            this.lblGioitinh.Size = new System.Drawing.Size(67, 20);
+            this.lblGioitinh.TabIndex = 2;
+            this.lblGioitinh.Text = "Giới tính";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(120, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(146, 26);
+            this.textBox2.TabIndex = 4;
+            // 
+            // lblNgaysinh
+            // 
+            this.lblNgaysinh.AutoSize = true;
+            this.lblNgaysinh.Location = new System.Drawing.Point(317, 40);
+            this.lblNgaysinh.Name = "lblNgaysinh";
+            this.lblNgaysinh.Size = new System.Drawing.Size(82, 20);
+            this.lblNgaysinh.TabIndex = 5;
+            this.lblNgaysinh.Text = "Ngày sinh:";
+            // 
+            // lblDiachi
+            // 
+            this.lblDiachi.AutoSize = true;
+            this.lblDiachi.Location = new System.Drawing.Point(317, 91);
+            this.lblDiachi.Name = "lblDiachi";
+            this.lblDiachi.Size = new System.Drawing.Size(61, 20);
+            this.lblDiachi.TabIndex = 6;
+            this.lblDiachi.Text = "Địa chỉ:";
+            // 
+            // lblSdt
+            // 
+            this.lblSdt.AutoSize = true;
+            this.lblSdt.Location = new System.Drawing.Point(317, 172);
+            this.lblSdt.Name = "lblSdt";
+            this.lblSdt.Size = new System.Drawing.Size(102, 20);
+            this.lblSdt.TabIndex = 7;
+            this.lblSdt.Text = "Số điện thoại";
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(610, 40);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 29);
+            this.btnThem.TabIndex = 8;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(610, 97);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 27);
+            this.btnXoa.TabIndex = 9;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(610, 146);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 34);
+            this.btnSua.TabIndex = 10;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            // 
+            // rdbNam
+            // 
+            this.rdbNam.AutoSize = true;
+            this.rdbNam.Location = new System.Drawing.Point(120, 145);
+            this.rdbNam.Name = "rdbNam";
+            this.rdbNam.Size = new System.Drawing.Size(67, 24);
+            this.rdbNam.TabIndex = 11;
+            this.rdbNam.TabStop = true;
+            this.rdbNam.Text = "Nam";
+            this.rdbNam.UseVisualStyleBackColor = true;
+            // 
+            // rdbNu
+            // 
+            this.rdbNu.AutoSize = true;
+            this.rdbNu.Location = new System.Drawing.Point(220, 145);
+            this.rdbNu.Name = "rdbNu";
+            this.rdbNu.Size = new System.Drawing.Size(54, 24);
+            this.rdbNu.TabIndex = 12;
+            this.rdbNu.TabStop = true;
+            this.rdbNu.Text = "Nữ";
+            this.rdbNu.UseVisualStyleBackColor = true;
+            // 
+            // dtpNgaysinh
+            // 
+            this.dtpNgaysinh.Location = new System.Drawing.Point(416, 41);
+            this.dtpNgaysinh.Name = "dtpNgaysinh";
+            this.dtpNgaysinh.Size = new System.Drawing.Size(153, 26);
+            this.dtpNgaysinh.TabIndex = 13;
+            // 
+            // wbDiachi
+            // 
+            this.wbDiachi.Location = new System.Drawing.Point(416, 91);
+            this.wbDiachi.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbDiachi.Name = "wbDiachi";
+            this.wbDiachi.Size = new System.Drawing.Size(172, 69);
+            this.wbDiachi.TabIndex = 14;
+            // 
+            // btnReport
+            // 
+            this.btnReport.Location = new System.Drawing.Point(713, 39);
+            this.btnReport.Name = "btnReport";
+            this.btnReport.Size = new System.Drawing.Size(75, 30);
+            this.btnReport.TabIndex = 16;
+            this.btnReport.Text = "Report";
+            this.btnReport.UseVisualStyleBackColor = true;
+            this.btnReport.Click += new System.EventHandler(this.btnReport_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(713, 97);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 27);
+            this.btnExport.TabIndex = 17;
+            this.btnExport.Text = "Export Excel";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(700, 146);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(134, 34);
+            this.btnImport.TabIndex = 18;
+            this.btnImport.Text = "Import Excel";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // txbSdt
+            // 
+            this.txbSdt.Location = new System.Drawing.Point(423, 172);
+            this.txbSdt.Name = "txbSdt";
+            this.txbSdt.Size = new System.Drawing.Size(146, 26);
+            this.txbSdt.TabIndex = 19;
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.Location = new System.Drawing.Point(40, 239);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.RowHeadersWidth = 62;
+            this.DataGridView1.RowTemplate.Height = 28;
+            this.DataGridView1.Size = new System.Drawing.Size(767, 199);
+            this.DataGridView1.TabIndex = 20;
+            // 
+            // lblThongtin
+            // 
+            this.lblThongtin.AutoSize = true;
+            this.lblThongtin.Location = new System.Drawing.Point(40, 213);
+            this.lblThongtin.Name = "lblThongtin";
+            this.lblThongtin.Size = new System.Drawing.Size(75, 20);
+            this.lblThongtin.TabIndex = 21;
+            this.lblThongtin.Text = "Thông tin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 450);
+            this.Controls.Add(this.lblThongtin);
+            this.Controls.Add(this.DataGridView1);
+            this.Controls.Add(this.txbSdt);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnReport);
+            this.Controls.Add(this.wbDiachi);
+            this.Controls.Add(this.dtpNgaysinh);
+            this.Controls.Add(this.rdbNu);
+            this.Controls.Add(this.rdbNam);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.lblSdt);
+            this.Controls.Add(this.lblDiachi);
+            this.Controls.Add(this.lblNgaysinh);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblGioitinh);
+            this.Controls.Add(this.lblTenNV);
+            this.Controls.Add(this.lblMaNV);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMaNV;
+        private System.Windows.Forms.Label lblTenNV;
+        private System.Windows.Forms.Label lblGioitinh;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblNgaysinh;
+        private System.Windows.Forms.Label lblDiachi;
+        private System.Windows.Forms.Label lblSdt;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.RadioButton rdbNam;
+        private System.Windows.Forms.RadioButton rdbNu;
+        private System.Windows.Forms.DateTimePicker dtpNgaysinh;
+        private System.Windows.Forms.WebBrowser wbDiachi;
+        private System.Windows.Forms.Button btnReport;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.TextBox txbSdt;
+        private System.Windows.Forms.DataGridView DataGridView1;
+        private System.Windows.Forms.Label lblThongtin;
+    }
+}
+
---
+++ windows-programming-labs/2024801030013/Lab51/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using OfficeOpenXml;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Lab51
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReport_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Export Excel";
+            saveFileDialog.Filter = "Excel (*.xlsx)|*.xlsx|Excel 2003 (*.xls)|*.xls";
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    ExportExcel(saveFileDialog.FileName);
+                    MessageBox.Show("Xuất file  thành công");
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Xuất file không thành công\n" + ex.Message);
+                }
+            }
+        }
+        private void ExportExcel(string path)
+        {
+            Excel.Application application = new Excel.Application();
+            application.Application.Workbooks.Add(Type.Missing);
+            for(int i =0; i <= DataGridView1.ColumnCount; i++)
+            {
+                application.Cells[1, i + 1] = DataGridView1.Columns[i].HeaderText;
+            }
+            for(int i = 0; i < DataGridView1.Rows.Count; i++)
+            {
+                for(int j = 0; j < DataGridView1.Columns.Count; j++)
+                {
+                    application.Cells[i + 2, j + 1] = DataGridView1.Rows[i].Cells[j].Value;
+                }
+            }
+            application.Columns.AutoFit();
+            application.ActiveWorkbook.SaveCopyAs(path);
+            application.ActiveWorkbook.Saved = true;
+     
+        }
+        private void ImportExcel(string path)
+        {
+            using(ExcelPackage excelPackage = new ExcelPackage (new FileInfo(path)))
+            {
+                ExcelWorksheet excelWorksheet = excelPackage.Workbook.Worksheets[0];
+                DataTable dataTable = new DataTable();
+                for (int i = excelWorksheet.Dimension.Start.Column; i <= excelWorksheet.Dimension.End.Column;i++) 
+                {
+                    dataTable.Columns.Add(excelWorksheet.Cells[1, i].Value.ToString());
+
+                }
+                for (int i = excelWorksheet.Dimension.Start.Row+1; i <= excelWorksheet.Dimension.End.Row; i++)
+                {
+                    List<string> listRows = new List<string>();
+                    for (int j = excelWorksheet.Dimension.Start.Column; j <= excelWorksheet.Dimension.End.Column; j++)
+                    {
+                        listRows.Add(excelWorksheet.Cells[i, j].Value.ToString());
+                    }
+                    dataTable.Rows.Add(listRows.ToArray());
+                }
+                DataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Import Excel";
+            openFileDialog.Filter = "Excel (*.xlsx)|*.xlsx|Excel 2003 (*.xls)|*.xls";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    ExportExcel(openFileDialog.FileName);
+                    MessageBox.Show("Nhập file  thành công");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Nhập file không thành công\n" + ex.Message);
+                }
+            }
+        }
+    }
+}
